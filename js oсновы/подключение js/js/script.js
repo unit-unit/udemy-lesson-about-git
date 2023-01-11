3111a6b7a017f383086e9efbd52dfc61c3332c0e
Bohdan <jonathantellem@gmail.com>
2023-01-11 14:22:49 +0200

    first commit

---
+++ js oсновы/подключение js/js/script.js
@@ -0,0 +1,45 @@
+// console.log("hello from js file");
+
+// const carBodyWidth = 1300;
+// const carBodyLength = 950;
+
+// console.log("ширина автомобиля " + carBodyWidth, ", длина авто: " + carBodyLength);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(obj.age);
+
+// const arr = [1, 2, 3];
+// const newObj = {
+//     a: 5,
+//     b: 7
+// };
+// console.log(typeof arr);
+// console.log(typeof newObj);
+
+
+// alert('hi');
+// const result = confirm("+14");
+// const answer = prompt('13');
+// console.log(typeof(answer));
+
+// const answers = [];
+// answers[0] = prompt('имя', '');
+// answers[1] = prompt('фамилия', '');
+// answers[2] = prompt('возраст', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+// const userName = 'UNIT';
+// console.log(`привет ${userName}`);
+
+let incr = 10;
+let decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
